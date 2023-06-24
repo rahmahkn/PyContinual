26d8c5aa0be2e1d294c0a690aa681ed43e61975f
Rahmah Khoirussyifa' Nurdini <rahmah.nurdini@mekari.com>
2023-06-24 14:39:30 +0700

    move unused exp

--- src/utils.py
+++ src/utils.py
@@ -928,28 +928,28 @@ if __name__ == "__main__":
     #         visualize('', row['exp_id'], f"res/til_classification/nusacrowd/{row['exp_id']} - {row['backbone']}_{row['baseline']}_.txt/{row['backbone']}_{row['baseline']}_.txt", 'nusacrowd_all_random', 'nusacrowd')
     
     # create viz for backbone and baseline combination
-    list_create_viz = [
-        # multi_baseline
-        [['bert'], ['mtl', 'one', 'ncl', 'ewc', 'a-gem'], 'multi_baseline'],
-        [['bert_frozen'], ['one', 'a-gem', 'hat', 'ncl', 'ewc'], 'multi_baseline'],
+    # list_create_viz = [
+    #     # multi_baseline
+    #     [['bert'], ['mtl', 'one', 'ncl', 'ewc', 'a-gem'], 'multi_baseline'],
+    #     [['bert_frozen'], ['one', 'a-gem', 'hat', 'ncl', 'ewc'], 'multi_baseline'],
         
-        # multi_backbone
-        [['bert_frozen', 'bert'], ['a-gem'], 'multi_backbone'],
-        [['bert_frozen', 'bert'], ['ewc'], 'multi_backbone'],
-        [['bert_frozen'], ['hat'], 'multi_backbone'],
-        [['bert'], ['mtl'], 'multi_backbone'],
-        [['bert', 'bert_frozen'], ['ncl'], 'multi_backbone'],
-        [['bert', 'bert_frozen'], ['one'], 'multi_backbone']
-    ]
+    #     # multi_backbone
+    #     [['bert_frozen', 'bert'], ['a-gem'], 'multi_backbone'],
+    #     [['bert_frozen', 'bert'], ['ewc'], 'multi_backbone'],
+    #     [['bert_frozen'], ['hat'], 'multi_backbone'],
+    #     [['bert'], ['mtl'], 'multi_backbone'],
+    #     [['bert', 'bert_frozen'], ['ncl'], 'multi_backbone'],
+    #     [['bert', 'bert_frozen'], ['one'], 'multi_backbone']
+    # ]
     
-    for elmt in list_create_viz:
-        run_create_viz(elmt[0], elmt[1], 'nusacrowd all random', elmt[2])
+    # for elmt in list_create_viz:
+    #     run_create_viz(elmt[0], elmt[1], 'nusacrowd all random', elmt[2])
     
     # recalculate an experiment
     # calculate_metrics(81, 'bert_adapter', 'a-gem')
     
     # recalculate all experiments        
-    # calculate_metrics(list_exp.iterrows())
+    calculate_metrics(list_exp.iterrows())
     
     # get worst forgetting
     # with open('res/til_classification/result_transfer_cl.csv', 'a', newline='') as fp:
